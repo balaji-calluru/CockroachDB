535ec03a874cd7c542a3b49bf68d95b68f40a01b
Balaji Calluru <balaji.calluru@gmail.com>
2021-09-17 12:15:19 -0500

    enable CDB in json-sample.py

--- json-sample.py
+++ json-sample.py
@@ -3,9 +3,9 @@ import psycopg2
 import requests
 import time
 
-## conn = psycopg2.connect(database="jsonb_test", user="maxroach", host="localhost", port=26257)
-## conn.set_session(autocommit=True)
-## cur = conn.cursor()
+conn = psycopg2.connect(database="jsonb_test", user="maxroach", host="localhost", port=26257)
+conn.set_session(autocommit=True)
+cur = conn.cursor()
 
 # The Reddit API wants us to tell it where to start from. The first request
 # we just say "null" to say "from the start"; subsequent requests will use
@@ -36,12 +36,12 @@ for n in range(1):
     # We structure our query so that we extract the `children` field, and then
     # expand that and insert each individual element into the database as a
     # separate row.
-##    cur.execute("""INSERT INTO jsonb_test.programming (posts)
-##            SELECT json_array_elements(%s->'data'->'children')""", (data,))
+    cur.execute("""INSERT INTO jsonb_test.programming (posts)
+           SELECT json_array_elements(%s->'data'->'children')""", (data,))
 
     # Reddit limits to 30 requests per minute, so do not do any more than that.
     time.sleep(2)
 
-##cur.close()
-##conn.close()
+cur.close()
+conn.close()
 f.close()
