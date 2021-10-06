43c97a9f8a7503c2d0bd14099ee30823b38579ef
Balaji Calluru <balaji.calluru@gmail.com>
2021-10-06 16:44:06 -0500

    no message

--- startDB.bat
+++ startDB.bat
@@ -3,3 +3,21 @@ start-node2.bat
 start-node3.bat
 
 init.bat
+
+
+
+
+
+Rem Pet insurance
+
+
+
+
+Rem DDD
+Rem Website UC
+Rem Mobile App UC
+Rem Mobile App Estimates
+Rem Blocked Documents
+Rem Workshop Material
+
+
